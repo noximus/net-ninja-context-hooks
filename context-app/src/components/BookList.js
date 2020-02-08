5b5911e95a54675329902b6586cdb6d27c02a7fc
Henry Richardson <hrichardson83@gmail.com>
2020-02-08 18:36:57 -0500

    component set up

---
+++ context-app/src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class BookList extends Component {
+  render() {
+    return (
+      <div>
+        <div className="book-list">
+          <ul>
+            <li>the wy of the kinds</li>
+            <li>the name of the wind</li>
+            <li>the last empire</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
